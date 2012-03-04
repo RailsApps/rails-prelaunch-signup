f5c7f9b0d00ec15f573dc3f54c4c24d3a4cffdfc
Daniel Kehoe <daniel@danielkehoe.com>
2012-03-03 17:38:58 -0800

    add route for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 RailsPrelaunchSignup::Application.routes.draw do
   
+  authenticated :user do
+    root :to => 'home#index'
+  end
 
   devise_scope :user do
     root :to => "devise/registrations#new"
