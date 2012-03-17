28411643c7e085331bab1f5e560fbed94173307f
Daniel Kehoe <daniel@danielkehoe.com>
2012-03-17 06:14:03 -0700

    add UserMailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = FactoryGirl.create(:user, email: "example@example.com")
+    @email = UserMailer.welcome_email(@user).deliver
+  end
+
+  it "should be delivered to the email address provided" do
+    @email.should deliver_to("example@example.com")
+  end
+
+  it "should contain the correct message in the mail body" do
+    @email.should have_body_text(/Welcome/)
+  end
+
+  it "should have the correct subject" do
+    @email.should have_subject(/Request Received/)
+  end
+
 end
