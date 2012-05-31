25bd2aed2f11cf4c2d12c8a43179cf4515b18bf1
Daniel Kehoe <daniel@danielkehoe.com>
2012-05-30 18:18:20 -0700

    add social sharing buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,9 +34,39 @@ $('document').ready(function() {
       data: dataString,
       success: function(data) {
         $('#request-invite').html(data);
+        loadSocial();
       }
     });
     return false;
   });
   
 })
+
+// load social sharing scripts if the page includes a Twitter "share" button
+function loadSocial() {
+    
+    //Twitter
+    if (typeof (twttr) != 'undefined') {
+      twttr.widgets.load();
+    } else {
+      $.getScript('http://platform.twitter.com/widgets.js');
+    }
+
+    //Facebook
+    if (typeof (FB) != 'undefined') {
+      FB.init({ status: true, cookie: true, xfbml: true });
+    } else {
+      $.getScript("http://connect.facebook.net/en_US/all.js#xfbml=1", function () {
+        FB.init({ status: true, cookie: true, xfbml: true });
+      });
+    }
+
+    //Google+
+    if (typeof (gapi) != 'undefined') {
+      $(".g-plusone").each(function () {
+        gapi.plusone.render($(this).get(0));
+      });
+    } else {
+      $.getScript('https://apis.google.com/js/plusone.js');
+    }
+}
