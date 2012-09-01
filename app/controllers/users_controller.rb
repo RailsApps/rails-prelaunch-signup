9637ec8b97a156b12c3fa290b68cdf2701363962
Daniel Kehoe <daniel@danielkehoe.com>
2012-09-01 12:56:50 -0700

    security fix suggested by Brakeman

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     authorize! :invite, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
     @user.send_confirmation_instructions
-    redirect_to :back, :notice => "Sent invitation to #{@user.email}."
+    redirect_to :back, :only_path => true, :notice => "Sent invitation to #{@user.email}."
   end
   
   def bulk_invite
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     users.each do |user|
       user.send_confirmation_instructions
     end
-    redirect_to :back, :notice => "Sent invitation to #{users.count} users."
+    redirect_to :back, :only_path => true, :notice => "Sent invitation to #{users.count} users."
   end
   
   private
