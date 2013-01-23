066b3e38afb99f02bd57f838daa26f971c49b7f5
Daniel Kehoe <daniel@danielkehoe.com>
2013-01-22 17:29:26 -0800

    build using rails_apps_composer 2.2.30

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-user.confirm!
-puts 'New user created: ' << user.name
-user2 = User.create! :name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
-user2.confirm!
-puts 'New user created: ' << user2.name
+# Environment variables (ENV['...']) are set in the file config/application.yml.
+# See http://railsapps.github.com/rails-environment-variables.html
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  puts 'role: ' << role
+end
+puts 'DEFAULT USERS'
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.name
 user.add_role :admin
-
