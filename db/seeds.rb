127d8a7977d36f3b4200a8b7210ba4b5da41890a
Daniel Kehoe <daniel@danielkehoe.com>
2013-01-22 18:10:59 -0800

    update seeds.rb to accommodate Devise Confirmable

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,5 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+user.skip_confirmation!
+user.save!
