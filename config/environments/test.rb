d656cf0aca924cb05abf2043ae048fcc2aac2dd2
Daniel Kehoe <daniel@danielkehoe.com>
2013-02-20 16:13:23 -0800

    build using rails_apps_composer 2.2.34

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ RailsPrelaunchSignup::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
 
