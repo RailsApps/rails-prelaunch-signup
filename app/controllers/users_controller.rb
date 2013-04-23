f0ef1bc63d26b25578af7990a18dca9c967d5da2
Daniel Kehoe <daniel@danielkehoe.com>
2013-04-22 17:18:52 -0700

    add missing methods in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destroy
+      redirect_to users_path, :notice => "User deleted."
+    else
+      redirect_to users_path, :notice => "Can't delete yourself."
+    end
+  end
+
   def invite
     authorize! :invite, @user, :message => 'Not authorized as an administrator.'
     @user = User.find(params[:id])
