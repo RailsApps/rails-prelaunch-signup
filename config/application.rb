67a62e23ab4cd1277ef4b3c169fc52d00632e52a
Will Schive <lexigram@goodworksonearth.org>
2015-04-13 20:46:40 -0700

    shifted env variables to config/local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,14 @@ Bundler.require(*Rails.groups)
 
 module RailsPrelaunchSignup
   class Application < Rails::Application
+    # http://railsapps.github.io/rails-environment-variables.html
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -30,6 +38,6 @@ module RailsPrelaunchSignup
       g.view_specs false
       g.helper_specs false
     end
-
+      
   end
 end
